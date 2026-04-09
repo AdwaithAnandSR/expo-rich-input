ac4bd95f663853db93c53d9df33fe6d79d15987c
AdwaithAnandSR <181568233+AdwaithAnandSR@users.noreply.github.com>
2026-04-09 08:09:09 +0530

    fix

--- android/src/main/java/expo/modules/richinput/ExpoRichInputView.kt
+++ android/src/main/java/expo/modules/richinput/ExpoRichInputView.kt
@@ -2,7 +2,6 @@ package expo.modules.richinput
 
 import android.content.Context
 import android.text.InputType
-import android.text.method.BaseInputConnection
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.*
